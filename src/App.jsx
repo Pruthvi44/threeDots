323dc3a8e1fab4747a40013776d8ee96f113af5b
shubhamv02 <shubhamvhanale.arag@gmail.com>
2024-05-13 16:07:14 +0530

    About Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css';
 import Home from './Components/Home';
 import Slider from './Components/Slider';
 import Header from './Components/Header';
-
+import About from '../src/Components/About/About'
 function App() {
   return (
     <>
     <div className='header'>
         <Header/>
         </div>
-      <Home/>
+      {/* <Home/> */}
+
+      <About />
     </>
   );
 }
---
+++ src/Components/About/Backdata.jsx
@@ -0,0 +1,26 @@
+import './Backdata.css'
+import image2 from '../../Assets/images/About_img/02.jpg';
+function Backdata(){
+    return(
+        <>
+
+
+        <div className='About_img-middle-content'>
+                        
+                <h1>01</h1><hr />
+                <h2>CREATIVE ADVERTISING AGENCY</h2>
+                <img src={image2} />
+        </div>
+
+        <div className='Backdata-mid-contect'>
+            <h3>3 Dots Design Pvt. Ltd. is a one-stop destination for every possible brand communications mandate.</h3>
+
+            <h4>During the times when the communication business is highly fragmented, we are one of the rare Advertising Agencies in Pune, promptly answering to the name of Integrated Communications Specialist.</h4>
+
+            <h4>So, we learn the brief. Interpret it. Unlearn it only till we ideate. Relook at it, imbibe it and then let it come to the fore in a smooth blend to capture the TG’s mind. This is a regular exercise we engage in at 3 Dots Design Pvt. Ltd.</h4>
+        </div>
+                    
+        </>
+    )
+}
+export default Backdata;
